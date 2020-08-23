364c755952300c15c09706e3735de621dcbae9d8
MandyHL <1033590016@qq.com>
2020-08-23 16:02:06 +0800

    修复求数组A中长度为N的组合回溯不完全的bug

--- 04.algorithm/求数组A中长度为N的组合.js
+++ 04.algorithm/求数组A中长度为N的组合.js
@@ -4,45 +4,50 @@ N = 3
 [[1, 2, 3], [1, 2, 4], [1, 2, 5], .... ] */
 
 function group(A, N) {
-// 请补全代码
+    // 请补全代码
     let result = [];
-    let childResult=[];
-    for(let i = 0 ;i < A.length; i++){
-        if(A.length >= N){
-            childResult = Object.assign([],createTree(i,A,N));
-            if(i === 2) debugger
-            console.log('childResult',childResult)
-            result = [...childResult,...result]
-            console.log('result',result)
-            A.shift()
-        }else{
-           break;
+    let childResult = [];
+    if (N === 0) {
+        return [];
+    } else if (N === 1) {
+        result = A.map(item => [item])
+    } else if (A.length === N) {
+        result = [A]
+    }else {
+        for (let i = 0; i < A.length; i++) {
+            if (A.length >= N && A.length - i >= N) {
+                childResult = Object.assign([], createTree(i, A, N));//数组是引用类型，注意深浅拷贝
+                result = [...result, ...childResult]
+            } else {
+                break;
+            }
         }
     }
     return result
 }
-function createTree(index,arr,N){
+function createTree(index, arr, N) {
     const result = [];
     let tempArr = [];
-    loop = function(index){
-        for(let i = index; i < arr.length ; i++){
+    loop = function (index) {
+        for (let i = index; i < arr.length; i++) {
             tempArr.push(arr[i]);//存字符串到数组
-           if(tempArr.length === N){//存储到足够的字符串，保存到结果集中
-               result.push(JSON.parse(JSON.stringify(tempArr)));
-               tempArr.pop();
-               if(i === arr.length - 1){
-                  let obj = tempArr.pop();
-                  if(tempArr.length){
-                      let index = arr.findIndex(item => item === obj);
-                      if(index > -1 && arr.length - index + tempArr.length >= N){
-                         loop(index + 1 )
-                      }else{
-                          return result
-                      }
-                  }
-               }
-    
-           }
+            if (tempArr.length === N) {//存储到足够的字符串，保存到结果集中
+                result.push(JSON.parse(JSON.stringify(tempArr)));//数组是引用类型，注意深浅拷贝
+                tempArr.pop();
+                if (i === arr.length - 1) {//指针到达最后位置，向上回溯
+                    uploop = function(){
+                        let obj = tempArr.pop();
+                    let index = arr.findIndex(item => item === obj);
+                        if (index > -1 && arr.length - index + tempArr.length >= N) {//还有足够的字符串再继续循环
+                            loop(index + 1)
+                        }
+                    }
+                    uploop()
+                    if (tempArr.length) {
+                        uploop()
+                    }
+                }
+            }
         }
         return result;
     }
@@ -50,6 +55,6 @@ function createTree(index,arr,N){
 
     return result;
 }
-group([ 1,2, 3, 4, 5],4)
+console.log(group([1, 2, 3, 4, 5,6,7], 4))
 
 
