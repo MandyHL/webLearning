5870d5b9dee48c0ee348a39ad0c5699a4871e517
Mandy <1033590016@qq.com>
2020-10-18 17:57:16 +0800

    fix

--- jsLearning/newFunction.js
+++ jsLearning/newFunction.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-10-18 17:36:47
- * @LastEditTime: 2020-10-18 17:50:25
+ * @LastEditTime: 2020-10-18 17:57:02
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \webLearning\jsLearning\newFunction.js
@@ -11,15 +11,15 @@ function create() {
     // 1.创建一个全新的对象
     let obj = new Object();
 
-    //2.获取构造函数
+    //获取构造函数
     let Con = [].shift.call(arguments);//拿到第一个元素
 
-    // 3.将对象的__proto__连接到构造函数的prototype
+    // 2.将对象的__proto__指向构造函数的prototype对象
     obj.__proto__ = Con.prototype;
 
-    // 4.绑定this, 以及参数;
+    // 3.绑定this, 以及参数;
     let result = Con.apply(obj,arguments)
     
-    //确保返回的是对象
+    //4.确保返回的是对象（如果构造器没有返回其他对象，则自动返回这个新对象）
     return typeof result === 'object' ? result : obj;
 }
